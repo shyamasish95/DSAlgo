2f863dacd7727e2934b8093037ddb4040ca49259
Shyamasish Mohanty <Shyamasish_Mohanty@epam.com>
2020-09-28 08:04:49 +0530

    commit initial

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace PracticeProj
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+		int[] freq = new int[256];
+		string s = "geeksforgeeks";
+        char[] temp = s.ToCharArray();
+		int n = temp.Length;
+		for(int i = 0;i<n;i++){
+		    freq[temp[i]]++;
+		}
+		for(int i =0; i< 256;i++){
+		    for(int j=0; j<freq[i];j++){
+		        Console.WriteLine((char)i);
+		    }
+		}
+        }
+    }
+}
